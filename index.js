155de51acc7e497891a45c64fa8edc56993308e6
brabrahen <brabrahen@gmail.com>
2021-11-23 22:35:59 -0300

    variaveis de ambiente e conexão dbatlas

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
 const express = require('express');
 const cors = require('cors');
 const Conn = require('./conn/conn');
@@ -10,9 +15,13 @@ const TodoRouter = require('./routes/todo.routes')
 
 app.use('/todo', TodoRouter);
 
-Conn();
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3001;
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Servidor rodando na http://localhost:3001/`)
 })
